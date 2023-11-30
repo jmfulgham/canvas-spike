8d14ba18f2a1e07e3e1deefb3fb3d9d5aab4ff17
Tim Etler <tmetler@gmail.com>
2023-11-30 11:17:06 -0800

    Added port message to server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // Socket server code goes here
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 8000;
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 8000;
+
 server.listen(port);
-app.use(express.static('./public'));
+app.use(express.static("./public"));
+console.info(`Server listening on port ${port}`);
